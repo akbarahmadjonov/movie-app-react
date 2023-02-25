6863e9c3e6b83c21ab636c5872f307a1040c9d09
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-02-26 00:21:37 +0500

    Add details banner, change styles, search

--- src/pages/home/topRated/TopRated.jsx
+++ src/pages/home/topRated/TopRated.jsx
@@ -1,26 +1,36 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
+import Carousel from "../../../components/carousel/Carousel";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import { SwitchTabs } from "../../../components/switchTabs/SwitchTabs";
-import { Carousel } from "../../../components/carousel/Carousel";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
-import { useFetch } from "../../../hooks/useFetch";
+import useFetch from "../../../hooks/useFetch";
 
-export const TopRated = () => {
-  const [endpoint, setEndPoint] = useState("movie");
+const TopRated = () => {
+    const [endpoint, setEndpoint] = useState("movie");
 
-  const { data, loading } = useFetch(`/${endpoint}/top_rated`);
+    const { data, loading } = useFetch(`/${endpoint}/top_rated`);
 
-  const onTabChange = (tab) => {
-    setEndPoint(tab === "Movies" ? "movie" : "tv");
-  };
+    const onTabChange = (tab) => {
+        setEndpoint(tab === "Movies" ? "movie" : "tv");
+    };
 
-  return (
-    <div className="carouselSection">
-      <ContentWrapper>
-        <span className="carouselTitle">Top Rated</span>
-        <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
-      </ContentWrapper>
-      <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
-    </div>
-  );
+    return (
+        <div className="carouselSection">
+            <ContentWrapper>
+                <span className="carouselTitle">Top Rated</span>
+                <SwitchTabs
+                    data={["Movies", "TV Shows"]}
+                    onTabChange={onTabChange}
+                />
+            </ContentWrapper>
+            <Carousel
+                data={data?.results}
+                loading={loading}
+                endpoint={endpoint}
+            />
+        </div>
+    );
 };
+
+export default TopRated;
